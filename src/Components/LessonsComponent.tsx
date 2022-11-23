611fb54b69e6b6abed7a0f4b2103bdf55710f900
Myroslav <myros.photo@gmail.com>
2022-11-23 21:00:58 +0100

    2

--- src/Components/LessonsComponent.tsx
+++ src/Components/LessonsComponent.tsx
@@ -5,13 +5,13 @@ const LessonsComponent = () => {
     return (
         <>
         <Grid templateColumns='repeat(7, 1fr)' gap={2}>
-        <GridItem w='100%' h='10' bg='blue.500'>Pon</GridItem>
-        <GridItem w='100%' h='10' bg='blue.500' />
-        <GridItem w='100%' h='10' bg='blue.500' />
-        <GridItem w='100%' h='10' bg='blue.500' />
-        <GridItem w='100%' h='10' bg='blue.500' />
-        <GridItem w='100%' h='10' bg='blue.500' />
-        <GridItem w='100%' h='10' bg='blue.500' />
+        <GridItem textAlign={"center"} w='100%' h='10' bg='blue.500'>Poniedziałek</GridItem>
+        <GridItem textAlign={"center"} w='100%' h='10' bg='blue.500'>Wtorek</GridItem>
+        <GridItem textAlign={"center"} w='100%' h='10' bg='blue.500'>Środa</GridItem>
+        <GridItem textAlign={"center"} w='100%' h='10' bg='blue.500'>Czwartek</GridItem>
+        <GridItem textAlign={"center"} w='100%' h='10' bg='blue.500'>Piątek</GridItem>
+        <GridItem textAlign={"center"} w='100%' h='10' bg='blue.500'>Sobota</GridItem>
+        <GridItem textAlign={"center"} w='100%' h='10' bg='blue.500'>Niedziela</GridItem>
       </Grid>
         </>
     );
